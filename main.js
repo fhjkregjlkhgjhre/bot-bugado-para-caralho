dca9b0f271b3fc2a06cbf507fc6bd24d1d1df253
lol-sys <68228498+lol-sys@users.noreply.github.com>
2020-07-19 03:59:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+const mineflayer = require("mineflayer");
+const discord = require("discord.js");
+const navigatePlugin = require('mineflayer-navigate')(mineflayer);
+const config = require("./config.json");
+
+// minecraft stuff
+const client = new discord.Client({autoReconnect: true}); //Caso que o discord me mande fuder na ligação
+const options = {
+    host: 'mc.hypixel.net',
+    port: 25565,
+    version: '1.8.9',
+    username: config["minecraft-username"],
+    password: config["minecraft-password"],
+};
+
+let mc;
+(function init() {
+    console.log("[WORKER] Minecraft -> Iniciando sessão na conta");
+    mc = mineflayer.createBot(options);
+    mc._client.once("session", session => options.session = session);
+    mc.once("end", () => {
+        setTimeout(() => {
+            console.log("Falhou ao entrar no server, reconenctando");
+            init();
+        }, 60000);
+    });
+}());
+
+let uuid;
+let name;
+mc.on("login", () => {
+    uuid = mc._client.session.selectedProfile.id;
+    name = mc._client.session.selectedProfile.name;
+    setTimeout(() => {
+    	console.log("[WORKER] Minecraft -> Ligação completa ao server")
+        console.log("[WORKER] Minecraft -> Indo para skyblock.");
+        mc.chat("/play sb");
+        bot.navigate.to(Vec3(, , ));
+    }, 1000);
+    
+});
+
+mc.on('kicked', function(reason) {
+  client.guilds.get(config["guild_id"]).channels.get("channel_id").send("> Desconectado do server\n", { reason });
+});
+
+mc.on("message", (chatMsg) => {
+    const msg = chatMsg.toString();
+    console.log(msg);
+    client.guilds.get(config["guild_id"]).channels.get("channel_id").send("> Messagem: Recebida\n", { msg });
+});
+
+// bot stuff
+client.on("ready", () => {
+    console.log("[WORKER] Discord -> ON");
+});
+
+client.login(config["discord-token"]);
