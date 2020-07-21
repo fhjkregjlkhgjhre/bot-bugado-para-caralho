4b55434b506462a4d7027c52931aad08a0b8e1d1
lol-sys <68228498+lol-sys@users.noreply.github.com>
2020-07-21 21:53:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function sendchat(ccn){
 }
 
 client.on('message', msg => {
-    if (msg.author.id === client.user.id) return;
+    if (msg.author.id === client.user.id && !msg.channel.id === config["channel_id"]) return;
     sendchat(msg.content);
     //const minecraft = minecraft
     //minecraft.send(msg.content);
