3297173f20e9c4cb0695c60f199e792cfc32e010
kjahkjhlkelryeufhjkrjlgerg <68228498+fhjkregjlkhgjhre@users.noreply.github.com>
2020-07-23 02:00:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,48 +12,44 @@ client.on("ready", () => {
     console.log("[WORKER] Discord -> ON");
 });
 
-function createBot (){
-	const minecraft = mineflayer.createBot({
-        	host: process.env.ipmine,
-        	port: 25565,
-        	version: '1.8.9',
-        	username: process.env.email,
-        	password: process.env.password,
-	});
-	client.on('message', msg => {
-    		if (msg.author.id === client.user.id) return;
-    		if (msg.content === 'sbafk') {
-			hypixelafk();
-        		msg.channel.send("Iniciando modo afk");
-		} else if (msg.content === 'quit') {
-			setTimeout(minecraft.quit(), 1000);
-        		msg.channel.send("Saindo do server");
-		} else if (msg.content === "join") {
-			setTimeout(createBot, 1000);
-        		msg.channel.send("Entrando no server"); 
-    		} else {
-	    		console.log(msg.content);
-            		minecraft.chat(msg.content);
-    		}
-	});
-	minecraft.on('login', () => console.log('logado'));
-	navigatePlugin(minecraft);
+const minecraft = mineflayer.createBot({
+            host: process.env.ipmine,
+            port: 25565,
+            version: '1.8.9',
+            username: process.env.email,
+            password: process.env.password,
+});
 
-	minecraft.on('kicked', function(reason) {
-  		client.channels.get(config["channel_id"]).send("> Desconectado do server\n" + reason);
-	});
-
-	minecraft.on("message", (chatMsg) => {
-    		try{
-        		const msg = chatMsg.toString();
-        		var canal = client.channels.get('735133986635907113');
-        		canal.send(msg);
-    		}catch(e){console.log("ERRO -> ",e)};
-	});
-	minecraft.on('error', function(err) {
-		console.log(err.code);
-    	});
+function hypixelafk(){
+	minecraft.chat("/play sb");
+	const pt = vec3(0 , 0, 0);
+	bot.navigate.to(pt);
 };
 
+client.on('message', msg => {
+    if (msg.author.id === client.user.id) return;
+    if (msg.content === 'sbafk') {
+        hypixelafk();
+            msg.channel.send("Iniciando modo afk");
+    }else {
+        console.log(msg.content);
+            sendchat(msg.content);
+    }
+});
+
+minecraft.on('login', () => console.log('logado'));
+navigatePlugin(minecraft);
+
+minecraft.on('kicked', function(reason) {
+  client.channels.get(config["channel_id"]).send("> Desconectado do server\n" + reason);
+});
+
+minecraft.on("message", (chatMsg) => {
+    try{
+        const msg = chatMsg.toString();
+        var canal = client.channels.get('735133986635907113');
+        canal.send(msg);
+    }catch(e){console.log("ERRO -> ",e)};
+});
+
 client.login(process.env.discordkey);
-createBot();
