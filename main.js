7f23f64da643d49c97c05117a9e41d730a4eb799
kjahkjhlkelryeufhjkrjlgerg <68228498+fhjkregjlkhgjhre@users.noreply.github.com>
2020-08-11 20:12:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -44,6 +44,10 @@ minecraft.on('kicked', function(reason) {
   client.channels.get(config["channel_id"]).send("> Desconectado do server\n" + reason);
 });
 
+minecraft.on('end'){
+	client.channels.get(config["channel_id"]).send("```css\n[Erro na coneçao com o server]\n```")
+};
+
 minecraft.on("message", (chatMsg) => {
     try{
     	console.log(chatMsg)
